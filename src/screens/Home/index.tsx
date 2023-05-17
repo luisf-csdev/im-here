c7ffcc14925aa7a9ef66a0e3b0e36a97019d1ecf
Luís Felipe <luisf.csdev@gmail.com>
2023-05-17 20:32:54 -0300

    data input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import { styles } from './styles'
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text style={styles.heading}>
+            <Text style={styles.eventName}>
                 Name of the event
             </Text>
-            <Text style={styles.date}>
+            <Text style={styles.eventDate}>
                 Monday, May 4th, 2023
             </Text>
+            <TextInput placeholder='Participant name'
+                placeholderTextColor={'#6B6D6B'}
+                style={styles.input} />
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,14 +6,22 @@ export const styles = StyleSheet.create({
         backgroundColor: '#131016',
         padding: 24
     },
-    heading: {
+    eventName: {
         color: '#FDFCFE',
         fontSize: 24,
         fontWeight: 'bold',
         marginTop: 48
     },
-    date: {
+    eventDate: {
         color: '#6B6B6B',
+        fontSize: 16
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
         fontSize: 16
     }
 })
